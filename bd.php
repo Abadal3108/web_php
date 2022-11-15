81708ad3635d94167d5b680d854b1d644fc5150a
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-11-15 18:18:26 +0100

    update 0.6

--- bd.php
+++ bd.php
@@ -12,19 +12,20 @@ $conexion = new PDO('mysql:host=fmesasc.com;dbname=daw2', 'daw2', 'Gimbernat');
 */
 /*
 $resultados = $conexion->query("CREATE TABLE web_grupo_alex_foro (
-    title varchar(255) PRIMARY KEY,    //añadir tabla description max size 60 adn title 30
+    title varchar(30) PRIMARY KEY,    
+    description varchar(30),
     content varchar(4000),
     image varchar(255)
 )");
 */
-/*
+
 $add = $conexion->query("insert into web_grupo_alex(name,mail,password,age,rol,image)
 values ('Alex','alex@gmail.com','alex13579',19,'admin','')");
-*/
 
-$add = $conexion->query("insert into web_grupo_alex_foro(title,content,image)
-values ('okvdzkvdl isjakfjdadad afmadkjfadk','blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablabla','')");
-/*
+
+$add = $conexion->query("insert into web_grupo_alex_foro(title,description,content,image)
+values ('Prueba1','descripcion','hola me llamo alex hola me llamo nil','')");
+
 $resulta = $conexion->query("SELECT * FROM web_grupo_alex ");
 foreach ($resulta as $fila) {
     echo $fila['name'] . '</br>';
@@ -33,15 +34,15 @@ foreach ($resulta as $fila) {
     echo $fila['mail'] . '</br>';
     echo $fila['rol'] . '</br>';
     echo $fila['image'] . '</br>';
-    echo '</br>';*/
-
+    echo '</br>';
+}
 
 $resulta = $conexion->query("SELECT * FROM web_grupo_alex_foro ");
 foreach ($resulta as $fila) {
     echo $fila['title'] . '</br>';
     echo $fila['content'] . '</br>';
+    echo $fila['description'] . '</br>';
     echo $fila['image'] . '</br>';
-
 
 }
 
