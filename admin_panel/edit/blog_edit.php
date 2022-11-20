ec66fec5818ba60e0af9d22174ae00a817e27885
Abadal3108 <113515441+Abadal3108@users.noreply.github.com>
2022-11-20 15:35:52 +0100

    update 0.97, mejoras

--- admin_panel/edit/blog_edit.php
+++ admin_panel/edit/blog_edit.php
@@ -7,8 +7,8 @@ $fileToUpload = $_FILES['fileToUpload'];
 $conexion = new PDO('mysql:host=fmesasc.com;dbname=daw2', 'daw2', 'Gimbernat');
 move_uploaded_file($_FILES['fileToUpload']['tmp_name'], '../../images/' . $_FILES['fileToUpload']['name']);
 
-$add = $conexion->query('insert into web_grupo_alex_foro(title,description,content,image,date)
-                                        values ("' . $title . '","' . $description . '","' . $content . '","' . $_FILES['fileToUpload']['name'] . '","' . date('Y-m-d', time()) . '")');
+
+$add = $conexion->query('update web_grupo_alex_foro set title="' . $title . '" , description= "' . $description . '", content="' . $content . '" , image="' . $_FILES['fileToUpload']['name'] . '" , date="' . date('Y-m-d', time()) . '" where title="' . $_POST['title'] . '"');
 
 
 header("Location: ../../Paginaciones/Paginacion_home/index.php");
